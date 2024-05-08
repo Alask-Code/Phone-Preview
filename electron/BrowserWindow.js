8e6ee4b35efbe32f95f1de8e8e0f5b69d71fc649
Alask-code <= anonimoplayer736@gmail.com>
2024-05-08 17:05:40 -0300

    adapt UI to Desktop

--- electron/BrowserWindow.js
+++ electron/BrowserWindow.js
@@ -2,9 +2,10 @@ const { BrowserWindow } = require('electron');
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 380,
+    height: 680,
     title: 'My App',
+    alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
